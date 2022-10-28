59de7335f210a0e7a81ef1fbdad04690e75eada5
krisdauliya <krishna_dauliya@yahoo.com>
2022-10-28 15:23:39 -0400

    tanssaction added

--- src/day2/bankActivity.java
+++ src/day2/bankActivity.java
@@ -4,19 +4,38 @@ import java.util.Scanner;
 
 public class bankActivity {
     static double balance = 100.00;
+    static int flag;                       // flag == 1 for deposit, flag == 0 for withdraw
+
     //Behaviour
         //Deposit
         //Withdraw
         //Interest
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("How much you what to deposit? ");
-        double deposit = input.nextDouble();
-        Deposit(deposit);
-        System.out.println(balance);
+        //Transaction
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to your Banking Portal");
+        System.out.println("Please Enter 1 for Deposit or 2 for Withdraw: ");
+        Scanner input = new Scanner(System.in);
+        flag = input.nextInt();
+        System.out.println("How much you what to deposit/Withdraw? ");
+        double deposit = input.nextDouble();
+        Transaction(deposit, flag);
+        System.out.println("Your running balance is: " + balance);
+    }
+    static void  Withdraw(double amount){
+        balance = balance - amount;
+        //return(balance);
     }
     static void  Deposit(double amount){
+        balance = balance+amount;
+        //return(balance);
+    }
+    static void  Transaction(double amount, int flag){
+        if ( flag == 1){
+            Deposit(amount);
+        }else {
+            Withdraw(amount);
+        }
         balance = balance+amount;
         //return(balance);
     }
